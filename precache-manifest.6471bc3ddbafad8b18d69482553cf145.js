3493fd64ee436d517e37ef5f62e3df4201c4a761
barathrajd <barathdeva757@gmail.com>
2020-07-03 23:04:50 +0530

    Updates

---
+++ precache-manifest.6471bc3ddbafad8b18d69482553cf145.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2482ac7c20ff633f220d7ec9cb15ff8",
+    "url": "/breaking/index.html"
+  },
+  {
+    "revision": "5265a8ddcdb3d526ae7b",
+    "url": "/breaking/static/css/main.2ecd3d97.chunk.css"
+  },
+  {
+    "revision": "6be1fb276052fd24c1eb",
+    "url": "/breaking/static/js/2.b1ceec11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking/static/js/2.b1ceec11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5265a8ddcdb3d526ae7b",
+    "url": "/breaking/static/js/main.98cb774b.chunk.js"
+  },
+  {
+    "revision": "a741caec01b2a3c8c278",
+    "url": "/breaking/static/js/runtime-main.19d4267a.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "807f040f3b3cc7332a7783040f1e5d5c",
+    "url": "/breaking/static/media/img.807f040f.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking/static/media/spinner.235b9b3e.gif"
+  }
+]);
